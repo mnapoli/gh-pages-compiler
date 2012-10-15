d98e504c4a3cdc532811c53e02271f48faaa9e68
Matthieu Napoli <matthieu.NOSPAM@mnapoli.fr>
2012-10-15 14:15:20 +0200

    First commit

---
+++ bin/gh-pages-compiler.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Usage:
+# gh-pages-compiler.sh <source-repo> <target-repo>
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <source-repo> <target-repo>" >&2
+  exit 1
+fi
+
+if [ -d tmp ]; then
+	rm -rf tmp
+fi
+
+git clone $1 tmp
+
+if [ ! -d output ]; then
+	mkdir output
+fi
+
+cat ../resources/template-header.htm > output/index.htm
+
+./github-flavored-markdown.rb tmp/README.md >> output/index.htm
+
+cat ../resources/template-footer.htm >> output/index.htm
+
+rm -rf tmp
