840bb9b6fc9c88e71fe149900851fcd8d5637cee
Matthieu Napoli <matthieu.NOSPAM@mnapoli.fr>
2012-10-16 10:12:30 +0200

    Template directory

--- bin/gh-pages-compiler.sh
+++ bin/gh-pages-compiler.sh
@@ -12,6 +12,8 @@ cd $(dirname $0)
 cd ..
 BASE_DIR=$(pwd)
 
+TEMPLATE_DIR=$BASE_DIR/resources/templates/default
+
 # Clean and create the directories
 if [ -d tmp ]; then
 	rm -rf tmp
@@ -30,21 +32,24 @@ find . -name '*.md' > $BASE_DIR/files.tmp
 
 cd $BASE_DIR
 
+# Copy resources files of the template
+cp -r $TEMPLATE_DIR/public/* output/
+
 # Compile each file
 while read line
 do
     echo $line
     mkdir -p $(dirname output/$line)
-	cat resources/template-header.htm > output/$line
+	cat $TEMPLATE_DIR/template-header.html > output/$line
 	bin/github-flavored-markdown.rb tmp/$line >> output/$line
-	cat resources/template-footer.htm >> output/$line
+	cat $TEMPLATE_DIR/template-footer.html >> output/$line
 done < files.tmp
 
 # Rename all files to .html
 find output -name "*.md" -exec rename '.md' '.html' {} \;
 
-# Rename 'readme.html' to 'index.html'
-find output -name "readme.html" -exec rename readme index {} \;
+# Rename 'README.html' to 'index.html'
+find output -name "README.html" -exec rename README.html index.html {} \;
 
 rm files.tmp
 rm -rf tmp
