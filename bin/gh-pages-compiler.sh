18d278ca4c1a1dd52bb082c335064541b0183739
Matthieu Napoli <matthieu@mnapoli.fr>
2012-11-10 22:18:59 +0100

    Shell script best practices

--- bin/gh-pages-compiler.sh
+++ bin/gh-pages-compiler.sh
@@ -15,7 +15,7 @@ cd $(dirname $0)
 cd ..
 BASE_DIR=$(pwd)
 
-TEMPLATE_DIR=$BASE_DIR/resources/templates/default
+TEMPLATE_DIR=${BASE_DIR}/resources/templates/default
 
 # Clean and create the directories
 if [ -d tmp ]; then
@@ -38,27 +38,27 @@ fi
 
 # Find all markdown files
 cd tmp
-find . -name '*.md' | rev | cut -f 2- -d '.' | rev > $BASE_DIR/files.tmp
+find . -name '*.md' | rev | cut -f 2- -d '.' | rev > ${BASE_DIR}/files.tmp
 
-cd $BASE_DIR
+cd ${BASE_DIR}
 
 # Copy resources files of the template
-if [ -d $TEMPLATE_DIR/public ]; then
-	cp -r $TEMPLATE_DIR/public/* output/
+if [ -d ${TEMPLATE_DIR}/public ]; then
+	cp -r ${TEMPLATE_DIR}/public/* output/
 fi
 
 # Compile each file
 while read line
 do
-	SOURCE_FILE=tmp/$line.md
-	OUTPUT_FILE=output/$line.html
+	SOURCE_FILE=tmp/${line}.md
+	OUTPUT_FILE=output/${line}.html
 	# Rename README to index
-	OUTPUT_FILE=$(echo $OUTPUT_FILE | sed -e 's/README/index/g')
-    echo $line
-    mkdir -p $(dirname output/$line)
-	cat $TEMPLATE_DIR/template-header.html > $OUTPUT_FILE
-	bin/github-flavored-markdown.rb $SOURCE_FILE >> $OUTPUT_FILE
-	cat $TEMPLATE_DIR/template-footer.html >> $OUTPUT_FILE
+	OUTPUT_FILE=$(echo ${OUTPUT_FILE} | sed -e 's/README/index/g')
+    echo ${line}
+    mkdir -p $(dirname output/${line})
+	cat ${TEMPLATE_DIR}/template-header.html > ${OUTPUT_FILE}
+	bin/github-flavored-markdown.rb ${SOURCE_FILE} >> ${OUTPUT_FILE}
+	cat ${TEMPLATE_DIR}/template-footer.html >> ${OUTPUT_FILE}
 done < files.tmp
 
 rm files.tmp
@@ -70,6 +70,6 @@ git add -A
 git commit --quiet -am  "Pages generation"
 git push --quiet
 
-cd $BASE_DIR
+cd ${BASE_DIR}
 
 rm -rf output
